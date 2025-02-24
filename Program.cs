be3714a19edf9f166396f459cbbac6c037a3305a
Zoey Hamm <339622581@GoTVDSB.ca>
2025-02-24 14:04:41 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_5___Assignement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
